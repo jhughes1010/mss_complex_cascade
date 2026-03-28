eb714cd4ee311b5e62070d57be5584ddaba1ae6e
James Hughes <jhughes1010@gmail.com>
2026-03-27 19:04:22 -0600

    WIP get masks to light green

--- mast.ino
+++ mast.ino
@@ -1,8 +1,53 @@
-void SetMast(int mast,int status)
-{
-  switch(mast){
+void SetMast(int mast, int status) {
+  int green_bit, red_bit, yellow_bit;
+  int addr;
+  bool adv_blink;
+
+  adv_blink = (millis() / 1000) % 2;
+
+  switch (mast) {
+    case points_upper:
+      addr = 0x27;
+      green_bit = 7;
+      yellow_bit = 6;
+      red_bit = 5;
+      break;
+
+    case points_lower:
+      addr = 0x27;
+      green_bit = 7;
+      yellow_bit = 6;
+      red_bit = 5;
+      break;
+
     case main:
-    break;
+      addr = 0x27;
+      green_bit = 7;
+      yellow_bit = 6;
+      red_bit = 5;
+      break;
+
+    case diverge:
+      addr = 0x27;
+      green_bit = 7;
+      yellow_bit = 6;
+      red_bit = 5;
+      break;
   }
 
+  if (status == clear) {
+    //i2c_write(mast,green_bit);
+  } else if (status == adv) {
+    if (adv_blink) {
+      //i2c_write(mast,green_bit);
+    } else {  
+      //dark
+    }
+  } else if (status == apr) {
+    //i2c_write(mast,green_bit);
+  } else if (status == occ) {
+    //i2c_write(mast,green_bit);
+  } else {
+    //dark
+  }
 }
--- mss_complex_cascade.ino
+++ mss_complex_cascade.ino
@@ -2,17 +2,27 @@
 //James Hughes
 //03-27-2026
 
+#include <Wire.h>
+#include <Adafruit_MCP23X17.h>
+#include <Adafruit_RGBLCDShield.h>
+
+
 enum { points_upper,
        points_lower,
        main,
        diverge };
+
+enum { clear,
+       adv,
+       apr,
+       occ };
 
 
 
 
 void setup() {
   Serial.begin(9600);
-  //set i2c port expander DDR
+  //set i2c port expander DDR and pullups
 
   //set LCD i2c port expander
 }
@@ -23,11 +33,12 @@ void loop() {
 
   currentTime = millis();
   //Read inputs i2C
+
   //Read inputs optical
+
   //Process inputs
+
   //Set masts
-  //SetPoints(0);
-  //SetMain(0);
   SetMast(points_upper, 0);
   SetMast(points_upper, 0);
   SetMast(main, 0);
